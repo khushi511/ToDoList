50cef49557e607aa4820258f4b5728272dc4e10a
khushi511 <singh.khushboo511@gmail.com>
2018-07-09 14:06:54 +0530

    validation function added

--- src/infrastructure/utils.js
+++ src/infrastructure/utils.js
@@ -2,3 +2,12 @@ export const updateState = (self, obj) => {
 	let newObj = Object.assign({}, self.state, obj)
 	self.setState(newObj);
 }
+
+//Validation
+export const isEmptyOrNull = (state, propName, error, errorMessage) => {
+	if(state && !state[propName]){
+		error[propName] = errorMessage;
+		return true
+	}
+	return false
+}
